2dad2c7af1782870682c787fa70bc5201e45d2f9
Muhammad Sami Ullah <kk34samikhan@gmail.com>
2026-06-25 13:08:44 +0500

    Update

--- Team4Stack-NextJS/frontend/lib/auth/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as AdminLoginPage } from './AdminLoginPage';
-export { default as StackStoreAdminLoginPage } from './StackStoreAdminLoginPage';
-export { default as TeamAdminLoginPage } from './TeamAdminLoginPage';
-export { default as SuperAdminLoginPage } from './SuperAdminLoginPage';
-export { UnifiedAdminLoginPage } from './UnifiedAdminLoginPage';
